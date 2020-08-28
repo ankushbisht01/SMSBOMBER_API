a94d6ddb0f37815e8a35e46511af89e6c61195fe
ankushbisht01 <66681846+ankushbisht01@users.noreply.github.com>
2020-08-28 22:49:40 +0530

    Update settings.py

--- Bomber_API/settings.py
+++ Bomber_API/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '5whan&yjg*rs-fdho(aij=7*!2tk6bmn^^#n5k1los08zm7hj^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
